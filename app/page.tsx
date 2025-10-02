0003592e63b51bd348235917c6695b673461bf34
Andrew Dover <monkeypunk811@gmail.com>
2025-10-02 04:31:33 -0700

    Changed data structure for the dynamic elements, updated information

--- app/page.tsx
+++ app/page.tsx
@@ -5,34 +5,9 @@ import ProjectsSection, { Project } from "@/components/Projects";
 import StickyFooter  from "@/components/StickyFooter";
 import RollingTitle from "@/components/RollingTitle";
 import CourseMaterialsSection, { Course } from "@/components/CourseMaterialsSection";
+import data from "@/public/data.json"
 
-
-const courses: Course[] = [
-  {
-    code: "CS 146",
-    title: "Data Structures and Algorithms",
-    description: "Implementations of advanced tree structures, priority queues, heaps, directed and undirected graphs. Advanced searching and sorting techniques (radix sort, heapsort, mergesort, and quicksort). Design and analysis of data structures and algorithms. Divide-and-conquer, greedy, and dynamic programming algorithm design techniques.",
-    url: "https://catalog.sjsu.edu/preview_course_nopop.php?catoid=17&coid=157882",
-  },
-  {
-    code: "CMPE 120",
-    title: "Computer Organization and Architecture",
-    description: "Introduction to computer organization and architecture, system buses, internal memory and external memory, input/output, central processing unit CPU, instruction sets, CPU structure and function, RISC, control unit.",
-    url: "https://catalog.sjsu.edu/preview_course_nopop.php?catoid=10&coid=41920",
-  },
-  {
-    code: "MATH 32",
-    title: "Calculus 3",
-    description: "Functions of more than one variable, partial derivatives, multiple integrals and vector calculus. Graphical, algebraic and numerical methods of solving problems.",
-    url: "https://catalog.sjsu.edu/preview_course_nopop.php?catoid=10&coid=41254",
-  },
-  {
-    code: "CMPE 131",
-    title: "Software Engineering I",
-    description: "Why software engineering? What is software engineering? Software development lifecycle activities: project planning and management requirements analysis, requirement specification. Software design, software testing, verification, validation, and documentation. Software quality assurance and review techniques, software maintenance, team-based projects.",
-    url: "https://catalog.sjsu.edu/preview_course_nopop.php?catoid=10&coid=41926",
-  },
-];
+const courses: Course[] = data["Courses"]
 
 
 export default function Home() {
@@ -43,31 +18,7 @@ export default function Home() {
     { label: "Contact", href:"mailto:andrew.dover@gmail.com?subject=Inquiry" },
   ] as const;
 
-  const projects: Project[] = [
-  
-  {
-    title: "MagicMirror",
-    description: "An Asthetic smart mirror to improve productivity",
-    image: "/projects/Mirror.png",
-    tags: ["Node.js"],
-    links: {code: "https://github.com/you/portfolio" },
-  },
-  
-  
-  {
-    title: "Personal Portfolio",
-    description: "Sleek site built on Next.js for optimized CSR",
-    image: "/projects/site.png",
-    tags: ["Next.js", "React", "Tailwind"],
-    links: { code: "https://github.com/you/" },
-  },
-    {
-    title: "Liturgy.Display",
-    description: "Realtime slide voice recognition based presentation system.",
-    tags: ["FastAPI", "Vosk"],
-    links: {code: "https://github.com/adover06/liturgy.display" },
-  },
-];
+  const projects: Project[] = data["Projects"];
 
   return (
     <>  
