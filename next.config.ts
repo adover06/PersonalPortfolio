b0c7b1960ed7415b599adb623593cb4d2197ec1e
Andrew Dover <monkeypunk811@gmail.com>
2026-06-07 21:59:03 -0700

    CORS lite

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,31 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 
-  output: 'standalone'
-
+  output: 'standalone',
+  async headers() {
+    return [
+      {
+        source: "/api/tickets",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,POST,PUT,DELETE,OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
+        ],
+      },
+      {
+       
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "https://andrewdover.com" }, // Your restricted production domain
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
+        ],
+      },
+    ];
+  },
 
 };
+
+
 
 export default nextConfig;
